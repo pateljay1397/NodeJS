11172a0ce3e1c14808a8cce9362efb2a93f2760a
Jay Patel <pateljaypj1397@gmail.com>
2020-06-13 15:59:18 -0400

    Assignment 1

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -25,4 +25,22 @@ dishRouter.route('/')
     res.end('Deleting all dishes');
 });
 
+dishRouter.Router('/:dishId').all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('content-Type','text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the dishes to you!');
+})
+.post((req, res, next) => {
+    res.statusCode =403;
+    res.end('PUT operation not supported on /dishes');
+})
+.put((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting all dishes');
+});
 module.exports = dishRouter;
