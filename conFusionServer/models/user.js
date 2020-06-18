13f027e41684c6cbc7de248b1b7bfc7d5070ce9e
Jay Patel <pateljaypj1397@gmail.com>
2020-06-17 21:13:20 -0400

    Mongoose Population

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -4,7 +4,15 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 
 var User = new Schema({
-    admin:   {
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    admin: {
         type: Boolean,
         default: false
     }
